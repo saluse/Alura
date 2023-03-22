ff021dadf8ad917e3401973f0cb52bbfdffbea4c
Weverton Seixas <weverton.saluse@gmail.com>
2023-03-22 19:54:11 -0300

    Atualização

---
+++ JOINS.sql
@@ -0,0 +1,20 @@
+SELECT * FROM 
+    tabela_de_vendedores A
+INNER JOIN notas_fiscais B
+ON A.MATRICULA = B.MATRICULA;
+
+SELECT 
+    A.MATRICULA, 
+    A.NOME, COUNT(*) 
+FROM tabela_de_vendedores A
+INNER JOIN notas_fiscais B
+ON A.MATRICULA = B.MATRICULA
+GROUP BY A.MATRICULA, A.NOME;
+
+SELECT 
+    DISTINCT A.CPF, 
+    A.NOME, 
+    B.CPF 
+FROM tabela_de_clientes A
+LEFT JOIN notas_fiscais B 
+    ON A.CPF = B.CPF
